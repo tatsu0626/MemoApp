867a347e22dc3c70225d912f9508412a33728b30
yabetatsuya <yabe.tatsuya0626@gmail.com>
2018-01-06 21:27:43 +0900

    Style Setting

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbartitle} >MEMOT</Text>
+          </View>
+        </View>
+
+        <View style={styles.memolist}>
+          <View style={styles.memolistitem}>
+            <Text style={styles.memotitle}>講座のアイディア</Text>
+            <Text style={styles.memodate}>2018/1/10</Text>
+          </View>
+
+          <View style={styles.memolistitem}>
+            <Text style={styles.memotitle}>講座のアイディア</Text>
+            <Text style={styles.memodate}>2018/1/10</Text>
+          </View>
+
+          <View style={styles.memolistitem}>
+            <Text style={styles.memotitle}>講座のアイディア</Text>
+            <Text style={styles.memodate}>2018/1/10</Text>
+          </View>
+
+          <View style={styles.memolistitem}>
+            <Text style={styles.memotitle}>講座のアイディア</Text>
+            <Text style={styles.memodate}>2018/1/10</Text>
+          </View>
+
+          <View style={styles.memolistitem}>
+            <Text style={styles.memotitle}>講座のアイディア</Text>
+            <Text style={styles.memodate}>2018/1/10</Text>
+          </View>
+
+        </View>
+
+        <View>
+          <Text>
+            +
+          </Text>
+        </View>
+
       </View>
     );
   }
@@ -18,4 +59,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  appbar: {
+    position: 'absolute',
+    top : 0,
+    left:0,
+    right:0,
+    height: 60,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+   appbartitle: {
+     color:'#fff',
+     fontSize: 18,
+   },
 });
