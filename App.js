b37f2bf22be092e223363e1569570d51e1441c81
yabetatsuya <yabe.tatsuya0626@gmail.com>
2018-01-07 18:03:43 +0900

    Create memolist

--- App.js
+++ App.js
@@ -41,8 +41,8 @@ export default class App extends React.Component {
 
         </View>
 
-        <View>
-          <Text>
+        <View style={styles.memoaddbotton}>
+          <Text style={styles.memoaddbottonTitle}>
             +
           </Text>
         </View>
@@ -55,16 +55,41 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:78,
+    shadowColor:'#000',
+    shadowOffset: {width:0,height:0},
+    shadowOpacity: 0.9,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  memolist: {
+    backgroundColor: '#eee',
+    width: '100%',
+    flex: 1,
+  },
+  memolistitem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff'
+  },
+  memotitle: {
+    fontSize:18,
+    marginBottom: 4,
+  },
+  memodate: {
+     fontSize: 12,
+     color: '#b9b4b4',
   },
   appbar: {
     position: 'absolute',
     top : 0,
     left:0,
     right:0,
-    height: 60,
+    height: 78,
     paddingTop: 30,
     backgroundColor: '#265366',
     justifyContent: 'center',
@@ -74,4 +99,20 @@ const styles = StyleSheet.create({
      color:'#fff',
      fontSize: 18,
    },
+   memoaddbotton:{
+     position: 'absolute',
+     bottom: 32,
+     right: 32,
+     width: 48,
+     height: 48,
+     backgroundColor: '#f765a0',
+     borderRadius: 24,
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   memoaddbottonTitle: {
+     fontSize: 32,
+     lineHeight: 32,
+     color: 'white',
+   }
 });
